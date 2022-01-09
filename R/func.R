44cd45571cb10a1b074dfac54319c569fd57dbb0
Abulily <4079204+vschaftenaar@users.noreply.github.com>
2022-01-09 19:40:54 +0100

    Add files via upload

--- R/func.R
+++ R/func.R
@@ -1,15 +1,10 @@
 get_directory<- function(){
-  cmdArgs <- commandArgs(trailingOnly = FALSE)
-  needle <- "--file="
-  match <- grep(needle, cmdArgs)
-  if (length(match) > 0) {
-    # Rscript
-    x <- normalizePath(sub(needle, "", cmdArgs[match]))
-  } else {
-    # 'source'd via R console
-    x <- normalizePath(sys.frames()[[1]]$ofile)
-  }
-  return(dirname(x))
+tryCatch({
+  dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
+  },error=function(cond){
+    dir <- dirname(regmatches(commandArgs(), regexpr("(?<=^--file=).+", commandArgs(), perl=TRUE)))
+  })
+  return(dir)
 }
 
 
